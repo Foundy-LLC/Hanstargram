20985d4d7169264be63ca23571ad947a30a3476d
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-11-02 07:53:07 +0900

    구글 sign in 기능 구현

--- app/src/main/java/io/foundy/hanstargram/view/login/LoginActivity.kt
+++ app/src/main/java/io/foundy/hanstargram/view/login/LoginActivity.kt
@@ -1,9 +1,18 @@
 package io.foundy.hanstargram.view.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.TextView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
 import io.foundy.hanstargram.R
 import io.foundy.hanstargram.base.ViewBindingActivity
 import io.foundy.hanstargram.databinding.ActivityLoginBinding
@@ -13,15 +22,55 @@ class LoginActivity : ViewBindingActivity<ActivityLoginBinding>() {
 
     private val viewModel: LoginViewModel by viewModels()
 
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var signInLauncher: ActivityResultLauncher<Intent>
+
     override val bindingInflater: (LayoutInflater) -> ActivityLoginBinding
         get() = ActivityLoginBinding::inflate
+
+    companion object {
+        private const val TAG = "LoginActivity"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // TODO(민성): 이미 로그인 된 경우 홈 화면으로 전환하기
+        if (viewModel.signedIn) {
+            navigateToHomeView()
+        }
 
+        initSignInLauncher()
+        initGoogleSignInClient()
         initSignInButton()
+    }
+
+    private fun initSignInLauncher() {
+        val contracts = ActivityResultContracts.StartActivityForResult()
+        signInLauncher = registerForActivityResult(contracts) {
+            if (it.resultCode == RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+                try {
+                    val account = task.getResult(ApiException::class.java)
+                    viewModel.signInWith(account.idToken!!, ::onCompleteSignIn)
+                    Log.d(TAG, "Success google sign in: " + account.id)
+                } catch (e: ApiException) {
+                    showSnackBar(getString(R.string.failed_to_sign_in))
+                    Log.e(TAG, "Failed google sign in: " + e.message)
+                }
+            } else {
+                val resultCode = it.resultCode
+                showSnackBar(getString(R.string.failed_to_sign_in))
+                Log.e(TAG, "Failed google sign in code: $resultCode")
+            }
+        }
+    }
+
+    private fun initGoogleSignInClient() {
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
     }
 
     private fun initSignInButton() {
@@ -33,11 +82,28 @@ class LoginActivity : ViewBindingActivity<ActivityLoginBinding>() {
     }
 
     private fun signIn() {
-        // TODO(민성): 구현하기
+        val signInIntent: Intent = googleSignInClient.signInIntent
+        signInLauncher.launch(signInIntent)
+    }
+
+    private fun onCompleteSignIn(result: Result<Any>) {
+        if (result.isSuccess) {
+            navigateToHomeView()
+        } else {
+            showSnackBar(getString(R.string.failed_to_sign_in))
+            Log.e(TAG, "Failed firebase sign in: " + result.exceptionOrNull())
+        }
+    }
+
+    private fun showSnackBar(message: String) {
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT).show()
     }
 
     private fun navigateToHomeView() {
-        val intent = HomeActivity.getIntent(this)
+        val intent = HomeActivity.getIntent(this).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_TASK_ON_HOME
+        }
         startActivity(intent)
+        finish()
     }
 }
--- app/src/main/java/io/foundy/hanstargram/view/login/LoginViewModel.kt
+++ app/src/main/java/io/foundy/hanstargram/view/login/LoginViewModel.kt
@@ -1,8 +1,14 @@
 package io.foundy.hanstargram.view.login
 
 import androidx.lifecycle.ViewModel
+import io.foundy.hanstargram.repository.AuthRepository
 
 class LoginViewModel : ViewModel() {
 
+    val signedIn: Boolean
+        get() = AuthRepository.isSignedIn()
 
+    fun signInWith(idToken: String, onComplete: (result: Result<Any>) -> Unit) {
+        AuthRepository.signInWith(idToken, onComplete)
+    }
 }
