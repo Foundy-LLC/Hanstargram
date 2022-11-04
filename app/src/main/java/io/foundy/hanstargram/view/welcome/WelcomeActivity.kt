e83f8c567b5325ec0bab7a36015210e60fff7d5b
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-11-04 19:28:31 +0900

    이미지 선택  API 수정

--- app/src/main/java/io/foundy/hanstargram/view/welcome/WelcomeActivity.kt
+++ app/src/main/java/io/foundy/hanstargram/view/welcome/WelcomeActivity.kt
@@ -7,30 +7,34 @@ import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.LayoutInflater
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
+import androidx.activity.viewModels
+import androidx.core.graphics.drawable.toBitmap
 import androidx.core.widget.addTextChangedListener
 import io.foundy.hanstargram.base.ViewBindingActivity
 import io.foundy.hanstargram.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : ViewBindingActivity<ActivityWelcomeBinding>() {
 
-    private val fileChooserContract =
-        registerForActivityResult(ActivityResultContracts.GetContent()) { imageUri ->
-            if (imageUri != null) {
-                @Suppress("DEPRECATION")
-                val bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                    ImageDecoder.decodeBitmap(
-                        ImageDecoder.createSource(
-                            contentResolver,
-                            imageUri
-                        )
+    private val viewModel: WelcomeViewModel by viewModels()
+
+    private val pickMedia = registerForActivityResult(PickVisualMedia()) { imageUri ->
+        if (imageUri != null) {
+            @Suppress("DEPRECATION")
+            val bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                ImageDecoder.decodeBitmap(
+                    ImageDecoder.createSource(
+                        contentResolver,
+                        imageUri
                     )
-                } else {
-                    MediaStore.Images.Media.getBitmap(contentResolver, imageUri)
-                }
-                binding.profileImage.setImageBitmap(bitmap)
+                )
+            } else {
+                MediaStore.Images.Media.getBitmap(contentResolver, imageUri)
             }
+            binding.profileImage.setImageBitmap(bitmap)
         }
+    }
 
     override val bindingInflater: (LayoutInflater) -> ActivityWelcomeBinding
         get() = ActivityWelcomeBinding::inflate
@@ -53,9 +57,15 @@ class WelcomeActivity : ViewBindingActivity<ActivityWelcomeBinding>() {
                 binding.doneButton.isEnabled = it.isNotEmpty()
             }
         }
+
+        binding.doneButton.setOnClickListener {
+            val name = binding.userNameEditText.text.toString()
+            val bitmap = binding.profileImage.drawable?.toBitmap()
+            viewModel.sendInfo(name, bitmap)
+        }
     }
 
     private fun showImagePicker() {
-        fileChooserContract.launch("image/*")
+        pickMedia.launch(PickVisualMediaRequest(PickVisualMedia.ImageOnly))
     }
 }
---
+++ app/src/main/java/io/foundy/hanstargram/view/welcome/WelcomeViewModel.kt
@@ -0,0 +1,11 @@
+package io.foundy.hanstargram.view.welcome
+
+import android.graphics.Bitmap
+import androidx.lifecycle.ViewModel
+
+class WelcomeViewModel: ViewModel() {
+
+    fun sendInfo(name: String, profileImage: Bitmap?) {
+
+    }
+}
